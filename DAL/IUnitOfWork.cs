2a43992fde05c8e94b47aff58d73c2aa68feb882
pepper <peerplamel@gmail.com>
2021-01-09 17:59:55 +0200

    Added IRepository, Repository, IUnitOfWork

---
+++ DAL/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		//IRepository< Courses { get; }
+		//IAuthorRepository Authors { get; }
+
+		//int SaveChanges();
+	}
+}
+
