f0b63c23df0090536180a6438d8405f6c7f0cb87
pepper <peerplamel@gmail.com>
2021-01-30 21:34:00 +0200

    Added some files to start working on Student - Main Page
    - None of them is finished.
    - IndexTemplate.cshtml is just a reference to the original template of the List View, it's not part of the project.

---
+++ PL/Models/ExamViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models.Student
+{
+	public class ExamViewModel
+	{
+		public string Subject { get; set; }
+		public string Title { get; set; }
+		public TimeSpan Duration { get; set; }
+		public double? Grade { get; set; }
+	}
+}
