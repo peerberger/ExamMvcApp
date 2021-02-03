4e279d7966de2e6c14fcb1b39ef2c35f12bb41a3
pepper <peerplamel@gmail.com>
2021-02-03 23:34:38 +0200

    Modified Student Main Page (StudentController.Index())
    It reads succesfully from the database the exams of the registered Student, and displays 2 tables:
    Future Exams, with no grades.
    Past Exams, with grades.

--- PL/Controllers/StudentController.cs
+++ PL/Controllers/StudentController.cs
@@ -1,19 +1,85 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using DAL.IdentityData;
+using Microsoft.AspNetCore.Authorization;
+using DAL.Repositories.UnitOfWorks;
+using PL.Models.Shared;
+using PL.Models.Student;
+using PL.BLL;
 
 namespace PL.Controllers
 {
 	public class StudentController : Controller
 	{
-		// GET: StudentController
-		public ActionResult Index()
+		private readonly UserManager<AppUser> _userManager;
+		private readonly IUnitOfWork _unitOfWork;
+
+		public StudentController(
+			UserManager<AppUser> userManager,
+			IUnitOfWork unitOfWork)
 		{
-			return View();
+			_userManager = userManager;
+			_unitOfWork = unitOfWork;
 		}
+
+		// GET: StudentController
+		[Authorize]
+		public async Task<ActionResult> Index()
+		{
+
+			//ClearExams();
+			//SeedExams();
+
+			//var exam = _unitOfWork.Exams.GetByIdIncludingUsers(20);
+
+			//var student = await _userManager.GetUserAsync(User);
+
+			//exam.Users.Add(student);
+			//_unitOfWork.SaveChanges();
+			
+
+			//var users = _unitOfWork.Exams.GetAllIncludingStudents();
+
+
+
+			
+			var exams = _unitOfWork.Exams.GetByStudentIncludingGradesAsNoTracking(User);
+
+			var indexViewModel = StudentViewModelsGenerator.GenerateIndexViewModel(exams);
+
+			return View(indexViewModel);
+			//return View("IndexTemplate", exams);
+		}
+
+		private void SeedExams()
+		{
+			var exams = new List<Exam>
+			{
+				new Exam{Subject = "math",Title = "calculus",Duration = TimeSpan.FromMinutes(1)},
+				new Exam{Subject = "math",Title = "algebra",Duration = TimeSpan.FromMinutes(2)},
+				new Exam{Subject = "math",Title = "blabla",Duration = TimeSpan.FromMinutes(3)}
+			};
+
+			_unitOfWork.Exams.AddRange(exams);
+			_unitOfWork.SaveChanges();
+		}
+
+		private void ClearExams()
+		{
+			var exams = _unitOfWork.Exams.GetAll();
+
+			_unitOfWork.Exams.RemoveRange(exams);
+			_unitOfWork.SaveChanges();
+		}
+
+
 
 		// GET: StudentController/Details/5
 		public ActionResult Details(int id)
